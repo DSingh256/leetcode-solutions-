6279d2b45f7201ad9f51f901c884de2aebab4c75
DSingh256 <dhairyasingh70@gmail.com>
2026-07-09 23:23:22 +0530

    Time: 2677 ms (29.93%) | Memory: 19.2 MB (63.19%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.py
@@ -0,0 +1,21 @@
+class Solution:
+    def beautySum(self, s: str) -> int:
+        n = len(s)
+        total_beauty = 0
+        
+        for i in range(n):
+            freq = [0] * 26
+            for j in range(i, n):
+                freq[ord(s[j]) - ord('a')] += 1
+                
+                max_f = 0
+                min_f = float('inf')
+                
+                for f in freq:
+                    if f > 0:
+                        max_f = max(max_f, f)
+                        min_f = min(min_f, f)
+                
+                total_beauty += (max_f - min_f)
+                
+        return total_beauty
